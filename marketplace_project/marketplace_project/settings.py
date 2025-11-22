2ae9077f4da6db13021b5179b873b5287b8e9bd5
cyber-Artek <sambist161008@gmail.com>
2025-11-22 15:32:04 +0200

    update

--- marketplace_project/marketplace_project/settings.py
+++ marketplace_project/marketplace_project/settings.py
@@ -15,16 +15,10 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = True
 ALLOWED_HOSTS: list[str] = []
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_DB"),
-        "USER": os.environ.get("POSTGRES_USER"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-        "HOST": os.environ.get("POSTGRES_HOST"),
-        "PORT": os.environ.get("POSTGRES_PORT"),
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
